57244b59a82ab26b30063f110c08f7cc7dd097ef
DipDabDip <55196305+DipDabDip@users.noreply.github.com>
2021-10-21 15:05:48 +0100

    started making oop
    dont touch yet, still ooping

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 # Python 3 program for Elo Rating
 import math
 
+Max_Elo_Gain = 50
 
 class player:
-    def __init__(self, name, rating):
+    _id = 1
+    
+    def __init__(self, name):
         self.name = name
-        self.rating = int(rating)
+        self.elo = 1000
+        self.wins = 0
+        self.losses = 0
+        self.id = player._id
+        player._id
+        
+    def wins(self):
+        return (self.wins + self.losses)
+    
+   def gamewon(self, other):
+        #do the stuff to win a game here
+        
+        #calc probabilities of both players winning
+        P2 = Probability(self.elo, other.elo)
+        P1 = 1 - P2
+        
+        #calc elo gain/loss
+        elo_gain = (Max_Elo_Gain * (1 - P1))
+        elo_loss = (Max_Elo_gain * (0 - P2))
+        
+        #update values
+        self.elo = elo_gain
+        other.elo = elo_loss
+        self.wins += 1
+        other.losses += 1
 
 # Function to calculate the Probability
-def Probability(rating1, rating2):
-  
+def Probability(elo1, elo2):
     return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
   
   
